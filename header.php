c1ba078656934e92ddf7b43079ecba8f7a14a8a9
NLMartian <nl.martian@gmail.com>
2013-01-26 22:48:03 +0800

    先加个简单的导航栏。

--- header.php
+++ header.php
@@ -4,10 +4,17 @@
 	<title><?php wp_title( '-', true, 'right' ); echo _wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
-<?php wp_head() // For plugins ?>
+
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), _wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), _wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+	
+	<!-- 导入Jquery -->
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.js"></script>
+	<!-- 加载菜单效果的 JavaScriot -->
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/j-menu.js"></script>
+	
+	<?php wp_head() // For plugins ?>
 </head>
 
 <body class="<?php sandbox_body_class() ?>">
@@ -21,5 +28,25 @@
 
 	<div id="access">
 		<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
-		<?php sandbox_globalnav() ?>
+		<!--?php sandbox_globalnav() ?-->
+		
+		<!-- 分类菜单 START -->
+		<div class="menubar">
+			<ul class="menus" id="menus">
+				<?php
+					if (is_home()) {
+						$home_menu_class = 'current-cat';
+					} else {
+						$home_menu_class = 'cat-item';
+					}
+				?>
+				<li class="<?php echo($home_menu_class); ?>">
+					<a title="<?php _e('Home', 'default'); ?>" href="<?php echo get_settings('home'); ?>/"><?php _e('Home', 'default'); ?></a>
+				</li>
+				<?php wp_list_categories('depth=2&title_li=0&orderby=name&show_count=0'); ?>
+			</ul>
+		</div>
+		<!-- 分类菜单 END -->
+		<div style="clear:both;"></div>
+		
 	</div><!-- #access -->
