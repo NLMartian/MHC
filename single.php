5bde52497320281bd39f6a887e175daa64a18648
TonyHe <coolhzb@163.com>
2013-02-09 00:24:13 +0800

    single.php搞明白了，初步更新。

--- single.php
+++ single.php
@@ -13,7 +13,8 @@
 								<?php 
 								$args = array(
 									'orderby' => 'id',
-									'parent' => '5'
+									'parent' => 4,
+									'hide_empty' => 0
 									);
 								$categories = get_categories($args);
 								$inCount = 0;
@@ -38,31 +39,17 @@
 						</div>
 					</div>
 			</div>
-		
-			<div id="nav-above" class="navigation">
-				<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'sandbox' )) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
-			</div>
-
-<?php while ( have_posts() ) : the_post() ?>
-
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-				<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a>
-				</h2>
-				<div class="entry-content">
-				<?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
-				<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
-				</div>
-			</div><!-- .post -->
-
-<?php comments_template() ?>
-
-<?php endwhile; ?>
-			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'sandbox' )) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
-			</div>
-
+			
+			<?php the_post(); ?>
+				<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>" style="margin-top: 20px;">
+					<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a>
+					</h2>
+					<div class="entry-content">
+					<?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
+					<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
+					</div>
+				</div><!-- .post -->
+			
 		</div><!-- #content -->
 	</div><!-- #container -->
 	
