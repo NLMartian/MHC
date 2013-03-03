d73973b91e4a6a2a80fb2cfad5b0a8cc290b3669
TonyHe <coolhzb@163.com>
2013-03-03 22:15:02 +0800

    增添保险新闻，健康信息部份的实现

---
+++ category-5.php
@@ -0,0 +1,27 @@
+<?php get_header() ?>
+
+<div id="main">
+	<div id="container">
+		<div id="content">
+
+<?php while ( have_posts() ) : the_post() ?>
+
+			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+				<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a>
+				</h2>
+				<div class="entry-content">
+				<?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
+				<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
+				</div>
+			</div><!-- .post -->
+
+<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+	
+	<!--ÊúÖ±·Ö¸îÏß-->
+	<div id="vertical-separator"></div>
+	<?php get_sidebar('category5') ?>
+</div>
+<?php get_footer() ?>
---
+++ sidebar-category6.php
@@ -0,0 +1,39 @@
+	<div id="primary" class="sidebar" style="margin-top: 24px;">
+		<h3 style="margin-top: 0;"><?php _e("<!--:zh-->联系我们<!--:--><!--:en-->Contact us<!--:-->"); ?></h3>
+		<div id="contact-img">
+			<img src="<?php bloginfo('template_directory'); ?>/images/contact-us-per.png" alt="Contact us" />
+		</div>
+		<div id="contact-info">
+			<span id="contact-addr"><?php _e("<!--:zh-->上海<!--:--><!--:en-->Shanghai<!--:-->"); ?><span><br/>
+			<span id="contact-num">+86 15901980381</span><br/>
+			<span id="contact-email"><a href="/" title="<?php _e('Email', 'sandbox')?>"><?php bloginfo('admin_email')?></a></span> <br/>
+		</div>
+		<div id="contact-way">
+			<span><?php _e("<!--:zh-->在线联系我们<!--:--><!--:en-->Contact us online<!--:-->"); ?></span>
+			<img src="<?php bloginfo('template_directory');?>/images/contact-skype.png" alt="Skype" title="Skype" style="padding-left: 15px;"/>
+			<img src="<?php bloginfo('template_directory');?>/images/contact-msn.png" alt="MSN" title="MSN" style="margin-left: -8px;"/>
+		</div>
+	</div><!-- #primary .sidebar -->
+
+	<div id="secondary" class="sidebar">
+		<ul class="xoxo">
+			<li id="categories">
+				<h3 class="cornerTitle"><?php _e("<!--:zh-->健康信息<!--:--><!--:en-->Health Information<!--:-->"); ?></h3>
+				<ul class="cornerUl">
+					<?php 
+						global $post;
+						$args = array('numberposts' => 3, 'category' => 5, 'order' => 'DESC', 'orderby' => 'post_date');
+						$myposts = get_posts($args);
+
+						foreach($myposts as $post) :
+						setup_postdata($post);
+					?>
+					<li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="title"><?php the_title(); ?></a></li>
+					<?php endforeach; ?>
+					
+					<a href="<?php echo get_category_link(5); ?>" title="View all posts under Health information" class="more"><?php _e("<!--:zh-->更多>><!--:--><!--:en-->More>><!--:-->"); ?></a>
+
+				</ul>
+			</li>
+		</ul>
+	</div><!-- #secondary .sidebar -->
