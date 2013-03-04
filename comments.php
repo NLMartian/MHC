1df11ff8f1eba279be68812b674f4b7616e2d9d0
NLMartian <nl.martian@gmail.com>
2013-03-04 15:52:45 +0800

    定制评论页面

--- comments.php
+++ comments.php
@@ -26,20 +26,20 @@ foreach ( $comments as $comment )
 <?php $sandbox_comment_alt = 0 ?>
 
 				<div id="comments-list" class="comments">
-					<h3><?php printf($comment_count > 1 ? __('<span>%d</span> Comments', 'sandbox') : __('<span>One</span> Comment', 'sandbox'), $comment_count) ?></h3>
+					<!--h3><?php printf($comment_count > 1 ? __('<span>%d</span> Comments', 'sandbox') : __('<span>One</span> Comment', 'sandbox'), $comment_count) ?></h3-->
 
 					<ol>
 <?php foreach ($comments as $comment) : ?>
 <?php if ( get_comment_type() == "comment" ) : ?>
-						<li id="comment-<?php comment_ID() ?>" class="<?php sandbox_comment_class() ?>">
+						<!--li id="comment-<?php comment_ID() ?>" class="<?php sandbox_comment_class() ?>">
 							<div class="comment-author vcard"><?php sandbox_commenter_link() ?></div>
 							<div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'sandbox'),
 										get_comment_date(),
 										get_comment_time(),
 										'#comment-' . get_comment_ID() );
-										edit_comment_link(__('Edit', 'sandbox'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-<?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'sandbox') ?>
-							<?php comment_text() ?>
+										edit_comment_link(__('Edit', 'sandbox'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div-->
+<!--?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'sandbox') ?-->
+							<!--?php comment_text() ?-->
 						</li>
 <?php endif; // REFERENCE: if ( get_comment_type() == "comment" ) ?>
 <?php endforeach; ?>
@@ -99,21 +99,21 @@ foreach ( $comments as $comment )
 
 							<p id="comment-notes"><?php _e( 'Your email is <em>never</em> shared.', 'sandbox' ) ?> <?php if ($req) _e( 'Required fields are marked <span class="required">*</span>', 'sandbox' ) ?></p>
 
-							<div class="form-label"><label for="author"><?php _e( 'Name', 'sandbox' ) ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'sandbox' ) ?></div>
+							<div class="form-label"><label for="author"><?php _e("<!--:zh-->姓名<!--:--><!--:en-->Name<!--:-->") ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'sandbox' ) ?></div>
 							<div class="form-input"><input id="author" name="author" class="text<?php if ($req) echo ' required'; ?>" type="text" value="<?php echo $comment_author ?>" size="30" maxlength="50" tabindex="3" /></div>
 
-							<div class="form-label"><label for="email"><?php _e( 'Email', 'sandbox' ) ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'sandbox' ) ?></div>
+							<div class="form-label"><label for="email"><?php _e("<!--:zh-->邮箱<!--:--><!--:en-->Email<!--:-->") ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'sandbox' ) ?></div>
 							<div class="form-input"><input id="email" name="email" class="text<?php if ($req) echo ' required'; ?>" type="text" value="<?php echo $comment_author_email ?>" size="30" maxlength="50" tabindex="4" /></div>
 
-							<div class="form-label"><label for="url"><?php _e( 'Website', 'sandbox' ) ?></label></div>
+							<div class="form-label"><label for="url"><?php _e("<!--:zh-->手机<!--:--><!--:en-->Mobile<!--:-->") ?></label></div>
 							<div class="form-input"><input id="url" name="url" class="text" type="text" value="<?php echo $comment_author_url ?>" size="30" maxlength="50" tabindex="5" /></div>
 
 <?php endif // REFERENCE: * if ( $user_ID ) ?>
 
-							<div class="form-label"><label for="comment"><?php _e( 'Comment', 'sandbox' ) ?></label></div>
+							<div class="form-label"><label for="comment"><?php _e("<!--:zh-->留言<!--:--><!--:en-->Comment<!--:-->") ?></label></div>
 							<div class="form-textarea"><textarea id="comment" name="comment" class="text required" cols="45" rows="8" tabindex="6"></textarea></div>
 
-							<div class="form-submit"><input id="submit" name="submit" class="button" type="submit" value="<?php _e( 'Post Comment', 'sandbox' ) ?>" tabindex="7" /><input type="hidden" name="comment_post_ID" value="<?php echo $id ?>" /></div>
+							<div class="form-submit"><input id="submit" name="submit" class="button" type="submit" value="<?php _e("<!--:zh-->提交留言<!--:--><!--:en-->Post Comment<!--:-->") ?>" tabindex="7" /><input type="hidden" name="comment_post_ID" value="<?php echo $id ?>" /></div>
 
 							<div class="form-option"><?php do_action( 'comment_form', $post->ID ) ?></div>
 
