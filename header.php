de60c1b0de7c6d7fd79e86ff11b669d0e74dc470
TonyHe <coolhzb@163.com>
2013-03-11 20:02:43 +0800

    首先修改小的样式问题

--- header.php
+++ header.php
@@ -86,7 +86,7 @@
 		</div>
 		<div style="clear:both;"></div>
 
-		<div id="language-setting">
+		<div id="language-setting" style="margin-top: 0;">
 			<select id="language" onchange="changeLanguage(this.value)"?>">
 				<?php 
 					global $q_config;
