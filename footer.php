edf806be19b5ce4eed0b825951229b922d97fa86
NLMartian <nl.martian@gmail.com>
2013-05-28 11:09:33 +0800

    添加百度分析工具和privacy链接

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 			<span class="meta-sep">|</span>
 			<span id="site-map"><a href="#" title="" rel="designer">Site Map</a></span>
 			<span class="meta-sep">|</span>
-			<span id="privacy"><a href="#" rel="">Privacy</a></span>
+			<span id="privacy"><a href="<?php echo get_permalink(395); ?>" rel="">Privacy</a></span>
 			<span>Copyright©20013-2020Pacific Prime, All rights reserved.</span>
 		</div>
 	</div><!-- #footer -->
@@ -19,5 +19,11 @@
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/j-menu.js"></script>
 	<!-- 导入mhc.js -->
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/mhc.js"></script>
+	<!-- 百度统计代码 -->
+	<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6f515f6d03c839170b87afec4c9e7ae8' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </body>
 </html>
