dae3a5ac4e7403cf38ca5173b4658bbb32240b40
TonyHe <coolhzb@163.com>
2013-05-31 09:55:59 +0800

    update the foot part....

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
 			<span class="meta-sep">|</span>
 			<span id="site-map"><a href="http://www.myhealthcover.com/sitemap.html" title="Site Map" rel="designer">Site Map</a></span>
 			<span class="meta-sep">|</span>
-			<span id="privacy"><a href="#" rel="">Privacy</a></span>
-			<span>Copyright©20013-2020Pacific Prime, All rights reserved.</span>
+			<span id="privacy"><a href="<?php echo get_permalink(395); ?>" rel="">Privacy</a></span>
+			<span>Copyright©20013-2020My HealthCover, All rights reserved.</span>
 		</div>
 	</div><!-- #footer -->
 
@@ -19,5 +19,9 @@
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/j-menu.js"></script>
 	<!-- 导入mhc.js -->
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/mhc.js"></script>
+	<script type="text/javascript">
+		var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+		document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6f515f6d03c839170b87afec4c9e7ae8' type='text/javascript'%3E%3C/script%3E"));
+	</script>
 </body>
 </html>
