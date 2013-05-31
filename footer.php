b7fde185d01fac4d25ea87f95b41e87f7c8c8254
TonyHe <coolhzb@163.com>
2013-05-31 09:40:18 +0800

    foot part add site map support

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 		<div id="footer-info">
 			<span id="contract-us"><a href="http://www.myhealthcover.com/?page_id=31" title="" rel="generator">Contact Us</a></span>
 			<span class="meta-sep">|</span>
-			<span id="site-map"><a href="#" title="" rel="designer">Site Map</a></span>
+			<span id="site-map"><a href="http://www.myhealthcover.com/sitemap.html" title="Site Map" rel="designer">Site Map</a></span>
 			<span class="meta-sep">|</span>
-			<span id="privacy"><a href="<?php echo get_permalink(395); ?>" rel="">Privacy</a></span>
+			<span id="privacy"><a href="#" rel="">Privacy</a></span>
 			<span>Copyright©20013-2020Pacific Prime, All rights reserved.</span>
 		</div>
 	</div><!-- #footer -->
@@ -19,11 +19,5 @@
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/j-menu.js"></script>
 	<!-- 导入mhc.js -->
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/mhc.js"></script>
-	<!-- 百度统计代码 -->
-	<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6f515f6d03c839170b87afec4c9e7ae8' type='text/javascript'%3E%3C/script%3E"));
-</script>
-
 </body>
 </html>
